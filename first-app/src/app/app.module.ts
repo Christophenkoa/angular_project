51d09f5dc1b4fe397c8a1075b0e9eace276821b2
Nkoa Christophe <nkoachristophe@gmail.com>
2020-07-13 03:19:07 +0100

    initialization

---
+++ first-app/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModules
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
